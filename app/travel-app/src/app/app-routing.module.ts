bb3cdd6c9cc744fa207c1f7c7d57aea7d79a41ce
solarbeam <butkinas@gmail.com>
2019-05-09 21:11:27 +0300

    Add bootstrap with npm, edit routes and sidebar

--- app/travel-app/src/app/app-routing.module.ts
+++ app/travel-app/src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { FormsModule } from '@angular/forms';
 import { EnterTravelInfoComponent } from './enter-travel-info/enter-travel-info.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/trips', pathMatch: 'full' },
   { path: 'trips', component: MyTripsComponent },
   { path: 'detail/:id', component: TripDetailComponent },
   { path: 'reports', component: MessagesComponent },
