8baedc509ae0067a2f21850ac75af125860c1b9f
BenasGud <31999624+BenasGud@users.noreply.github.com>
2019-05-15 19:00:14 +0300

    Registracija
    Registracijos puslapis

--- app/travel-app/src/app/app.component.spec.ts
+++ app/travel-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'travel-app'`, () => {
+  it(`should have as title 'registracija'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('travel-app');
+    expect(app.title).toEqual('registracija');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to travel-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to registracija!');
   });
 });
---
+++ app/travel-app/src/app/mock-dbregist.ts
@@ -0,0 +1,8 @@
+import { Employee } from './employee';
+
+export const HEROES: Employee[] = [
+  { id: 36542, name: 'Tomas', surname: 'Tomauskas'},
+  { id: 78995, name: 'Jonas', surname: 'Jonauskas'},
+  { id: 69785, name: 'Viktoras', surname: 'Kazlauskas' },
+  { id: 45237, name: 'Juozas', surname: 'Juozaitis' },
+];
