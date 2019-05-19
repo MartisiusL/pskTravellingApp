4e7f95d9cfb8f17aff909451fe6a28a6db47ff81
Moduxx <31692532+Moduxx@users.noreply.github.com>
2019-05-19 21:06:10 +0300

    simple login page
    added simple login page

---
+++ app/travel-app/src/app/login-component/login-component.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login-component.component.html',
+  styleUrls: ['./login-component.component.css']
+})
+export class LoginComponentComponent implements OnInit {
+
+  constructor(private Auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  loginUser(event) {
+    event.preventDefault()
+    const target = event.target
+    const username = target.querySelector('#username').value
+    const password = target.querySelector('#password').value
+
+    this.Auth.getUserDetails(username, password)
+    console.log(username,password)
+  }
+}
